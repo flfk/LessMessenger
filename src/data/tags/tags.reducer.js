2a1328d9802854bfd4c1df8f350a911686428edb
flfk <flfk@users.noreply.github.com>
2019-01-15 11:33:09 -0300

    Minor change

--- src/data/tags/tags.reducer.js
+++ src/data/tags/tags.reducer.js
@@ -10,9 +10,7 @@ const initialState = {
 const reducerTags = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TAG.SUCCESS:
-      console.log('adding tag reducer');
       if (state.byId[action.payload]) return state;
-      console.log('adding new tag tag reducer');
       return {
         ...state,
         allIds: [...state.allIds, action.payload],
@@ -22,8 +20,6 @@ const reducerTags = (state = initialState, action) => {
         },
       };
     case TOGGLE_TAG.SUCCESS:
-      console.log('reducer toggle', action.payload);
-      console.log('state is currently', state.byId[action.payload]);
       return {
         ...state,
         byId: {
@@ -38,32 +34,5 @@ const reducerTags = (state = initialState, action) => {
       return state;
   }
 };
-
-const allIds = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TAG.SUCCESS:
-      if (state[action.payload]) return state;
-      return [...state, action.payload.id];
-    default:
-      return state;
-  }
-};
-
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case ADD_TAG.SUCCESS:
-      if (state[action.payload]) return state;
-      return { ...state, [action.payload.id]: action.payload };
-    case TOGGLE_TAG.SUCCESS:
-      return {};
-    default:
-      return state;
-  }
-};
-
-// const reducerTags = combineReducers({
-//   allIds,
-//   byId,
-// });
 
 export default reducerTags;
